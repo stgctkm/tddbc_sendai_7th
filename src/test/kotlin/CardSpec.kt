8f1dbf2ac5290af70a5211bf9654824c06060b8a
IMAI Kazuki <imaikazuki@gmx.com>
2017-09-09 14:17:50 +0900

    refactored

--- src/test/kotlin/CardSpec.kt
+++ src/test/kotlin/CardSpec.kt
@@ -7,12 +7,6 @@ import kotlin.test.*
 
 class CardSpec: Spek({
     describe("a card") {
-        on("クラス") {
-            it("が引数suitとrankをとる") {
-                assertNotNull(Card(Suit.SPADE, Rank.THREE))
-            }
-        }
-
         on("文字列表記") {
             it("「スペードの3」の文字列表記は「3♠」") {
                 assertEquals(Card(Suit.SPADE, Rank.THREE).getNotation(), "3♠")
@@ -39,14 +33,13 @@ class CardSpec: Spek({
             val aceOfSpades = Card(Suit.SPADE, Rank.ACE)
             val aceOfHearts = Card(Suit.HEART, Rank.ACE)
 
-            it ("スペードの3とスペードのAは異なるランクを持つ") {
-                assertFalse(threeOfSpades hasSameRank aceOfSpades)
-            }
             it ("スペードのAとハートのAは同じランクを持つ") {
                 assertTrue(aceOfSpades hasSameRank aceOfHearts)
             }
+            it ("スペードの3とスペードのAは異なるランクを持つ") {
+                assertFalse(threeOfSpades hasSameRank aceOfSpades)
+            }
         }
-
-    }
+   }
 })
 
